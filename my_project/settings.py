70ba9fef1c15138f0d7b09a91e4c2961c178e5e8
Charlotta Grundmann <lotta@grundmann.se>
2024-02-28 14:08:33 +0000

    Fix error in settings for Allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t(44i)n-cklpfqynhf_esc%e@#+qgspq!!6=d9jpyu0cb2e(jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-charlottag-djangog-8u6cg75hgwl.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-charlottag-djangog-8u6cg75hgwl.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello_world',
     'about',
-    '.herokuapp.com'
 ]
 
 MIDDLEWARE = [
